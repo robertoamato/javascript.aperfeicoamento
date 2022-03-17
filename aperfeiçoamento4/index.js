d79770654bfb40d27088c2efc1025365a581d775
Roberto amato neto <66858040+robertoamato@users.noreply.github.com>
2022-03-17 00:18:55 -0300

    exemplo de como aplicar o array find

---
+++ aperfeiçoamento4/index.js
@@ -0,0 +1,13 @@
+const fruits = [
+{name: "felipe", empresa:"google"},
+{name:"roberto", empresa:"apple"},
+{name:"bruno", empresa:"microsoft"},
+]
+
+const FoundFruit = fruits.find( fruit => fruit.name === "felipe" )
+
+console.log( FoundFruit )
+
+
+
+
